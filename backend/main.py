4f7d329a381b85c0abba2604ae1448105a98450e
Juan Josue Zuleta Beb <2971250870108@ingenieria.usac.edu.gt>
2023-12-08 16:12:12 -0600

    iniciando xml

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,27 @@
 from analizador.parser import parser
 
+from xml.dom import minidom
+import os
+
+filename = 'backend/structure.xml'
+
+# Verificar si el archivo ya existe
+if os.path.exists(filename):
+    pass
+else:
+    # Si no existe, crear la estructura y guardarla en el archivo
+    doc = minidom.Document()
+
+    root = doc.createElement('xsqldata')
+    doc.appendChild(root)
+
+    with open('backend/structure.xml', 'w', encoding='utf-8') as file:
+        doc.writexml(file, indent='\t', addindent='\t', newl='\n')
+
+    print("Structure XML created successfully")
+
 # f = open('backend/entrada.txt', 'r')
-f = open('./entrada2.txt', 'r')
+f = open('backend/entrada2.txt', 'r')
 input = f.read()
 # print(input)
 instrucciones = parser.parse(input)
