40e5a01eb4de4a63a36e290d04994d6017a8ada6
Juan Josue Zuleta Beb <2971250870108@ingenieria.usac.edu.gt>
2023-12-14 14:02:15 -0600

    correcion al update

--- backend/interprete/instrucciones/update.py
+++ backend/interprete/instrucciones/update.py
@@ -11,7 +11,27 @@ class Update(Instruccion):
         self.tupla = tupla                 
         self.condicion = condicion
     
-    def ejecutar(self, env:Enviroment):  
+    def ejecutar(self, env:Enviroment):
+        print('------------------ UPDATE --------------------------')
+        print("> Nombre de tabla: ", self.table_name)
+
+        print("> Tupla: (valores a asignar) ")
+        for tup in self.tupla:
+            print("     Campo: ", tup.id)
+            expresion = tup.expresion.ejecutar(env)
+            print("     Expresion: ", expresion.valor)
+
+        # Solo acepta condiciones del tipo: WHERE campo = expresion
+        print("> Valor de la condicion: ")
+        print("     Campo: ", self.condicion.id)
+        expresion = self.condicion.expresion.ejecutar(env)
+        print("     Expresion: ", expresion.valor)
+        print('-----------------------------------------------------')
+
+        
+
+
+
         with open('backend/structure.xml', 'r+', encoding='utf-8') as file:
             mydoc = minidom.parse(file)
             
@@ -23,19 +43,30 @@ class Update(Instruccion):
                     for table in database.getElementsByTagName('tables'):
                         for table in table.getElementsByTagName('table'):
                             if table.getAttribute('name') == self.table_name:
-                                fields = table.getElementsByTagName('fields')[0]
+                                ##encontrar el campo a actualizar
+                                ##UPDATE persona SET nombre = 'aaaaaa' WHERE nombre = 'juanito';
+                                ##UPDATE self.table_name SET tup.id = tup.expresion.ejecutar(env).valor WHERE self.condicion.id = self.condicion.expresion.ejecutar(env).valor;
+                                ###print('UPDATE ' + self.table_name + ' SET ' + tup.id + ' = ' + tup.expresion.ejecutar(env).valor + ' WHERE ' + self.condicion.id + ' = ' + self.condicion.expresion.ejecutar(env).valor + ';')
+                                found = 0
+                                cont_records = 0
+                                for recs in table.getElementsByTagName('records'):
+                                    for record in recs.getElementsByTagName('record'):
+                                        cont_records += 1
+                                        for rc in record.getElementsByTagName('field'):
+                                            #print(rc.getAttribute('name'),' =' , rc.firstChild.data)
+                                            if rc.getAttribute('name') == self.condicion.id and rc.firstChild.data == self.condicion.expresion.ejecutar(env).valor:
+                                                print(rc.getAttribute('name'), '=', self.condicion.id)
+                                                print(rc.firstChild.data, 'es?', self.condicion.expresion.ejecutar(env).valor)
+                                                found = cont_records
+                                                break
 
-                                for atribute in fields.getElementsByTagName('field'):
-                                    if atribute.getAttribute('name') == self.condicion.id:
-                                        for recs in table.getElementsByTagName('records'):
-                                            for rc in recs.getElementsByTagName('field'):
-                                                for tup in self.tupla:
-                                                    if rc.getAttribute('name') == tup.id:
-                                                        if rc.getAttribute('param1') == 'TipoOpciones.PRIMARYKEY' or rc.getAttribute('param2') == 'TipoOpciones.PRIMARYKEY':
-                                                            print("Error: No se puede actualizar la llave primaria")
-                                                            return
-                                                        rc.firstChild.data = tup.expresion.ejecutar(env).valor
-        
+                                for rc in  table.getElementsByTagName('records')[0].getElementsByTagName('record')[found-1].getElementsByTagName('field'):
+                                    for tup in self.tupla:
+                                        if rc.getAttribute('name') == tup.id:
+                                            print(rc.getAttribute('name'), '=', tup.id)
+                                            rc.firstChild.data = tup.expresion.ejecutar(env).valor
+                                            break
+     
             xml_str = mydoc.toxml(encoding='utf-8').decode('utf-8').replace('\n', '').replace('\t', '')
             formatted_xml = minidom.parseString(xml_str).toprettyxml(indent="\t", encoding='utf-8').decode('utf-8')
             file.seek(0)
